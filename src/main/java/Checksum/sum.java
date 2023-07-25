fe86bce1145a6c46970ce15cf4836a23ed5cc4bd
nightfoxbmg <mishabalabanpw@gmail.com>
2023-07-25 18:11:39 +0300

    checksum add

---
+++ src/main/java/Checksum/sum.java
@@ -0,0 +1,11 @@
+package Checksum;
+
+public class sum
+        public static void CheckSign(int a, int b, int c) {
+    int sum = a + b + c;
+    if(sum >= 0)
+        System.out.println("Positive sum");
+    else
+        System.out.println("Negative sum");
+}
+}
